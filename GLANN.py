e92556b8a4ae29cbb1381a44c38082e5b9b5afe0
Administrator <admin@example.com>
2019-12-25 14:35:46 +0800

    save

--- GLANN.py
+++ GLANN.py
@@ -77,7 +77,7 @@ class GLANN(op_base):
     def perceptual_loss(self, gen, origin):
         alpha1 = 1.
         alpha2 = 1.
-        _imle_deep = tf.shape(gen)[0]
+        _imle_deep = gen.get_shape().as_list()[0]
         ### content
         gen_conv3_content = self.vgg(gen,conv3 = True) ## imle_deep, h,w,c
         origin_conv3_content = self.vgg(origin,conv3 = True)  ## 1, h,w,c
@@ -127,7 +127,7 @@ class GLANN(op_base):
         perceptual_loss = self.perceptual_loss(imle_choose_img, self.input_image)
         imle_gen_loss = tf.reduce_min(perceptual_loss)
         imle_gen_mean_loss = tf.reduce_mean(perceptual_loss)
-        
+
         self.summaries.append(tf.summary.scalar('g_min_loss',imle_gen_loss)) 
         self.summaries.append(tf.summary.scalar('g_mean_loss',imle_gen_mean_loss)) 
         
